9eeaff9cbd4ee9c1460decde17dc9b7bf9cae463
Matheus Oliveira <matheus.6148@gmail.com>
2024-06-12 19:11:39 -0300

    Caixinha.java (Node)

---
+++ src/ListasEncadeadas/Caixinha.java
@@ -0,0 +1,22 @@
+package ListasEncadeadas;
+
+public class Caixinha {
+    private int elemento;
+    private int proximo;
+
+    public void setElemento(int elemento){
+        this.elemento = elemento;
+    }
+
+    public int getElemento(){
+        return this.elemento;
+    }
+
+    public void setProximo(Caixinha proximo){
+        this.proximo = proximo;
+    }
+
+    public Caixinha getProximo(){
+        return this.proximo;
+    }
+}
